6f3b9eb2c42f513f40db96456ccc433f929b8676
seongmin <aeternussm@gmail.com>
2021-05-13 23:57:47 +0900

    시놉시스를 명사 단위로 쪼개 사전 벡터에 추가 코모란 형태소 분석기 사용

--- src/main/java/project/sec/util/dictionary.java
+++ src/main/java/project/sec/util/dictionary.java
@@ -1,5 +1,9 @@
 package project.sec.util;
 
+import kr.co.shineware.nlp.komoran.constant.DEFAULT_MODEL;
+import kr.co.shineware.nlp.komoran.core.Komoran;
+import kr.co.shineware.nlp.komoran.model.KomoranResult;
+import kr.co.shineware.nlp.komoran.model.Token;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -43,6 +47,18 @@ public class dictionary {
             }
             for (String s : tmp) {
                 set.add(s);
+            }
+
+            Komoran komoran = new Komoran(DEFAULT_MODEL.FULL);
+            if (m.getOutline().length() >= 1) {
+                KomoranResult list = komoran.analyze(m.getOutline());
+                List<Token> tokenList = list.getTokenList();
+                for (Token token : tokenList) {
+                    if (token.getPos().equals("NNP")) {
+                        System.out.println(token.getMorph());
+                        set.add(token.getMorph());
+                    }
+                }
             }
         }
 
