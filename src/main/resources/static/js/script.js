33d0013207c5126a78ee47226f2c92d221c510ab
oyfkr <oyfkr@naver.com>
2021-06-02 14:51:32 +0900

    pull을 위한 커밋

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -9,71 +9,70 @@
  */
 
 ;(function ( $, window, document, undefined ) {
-    let slides = document.querySelector('.slides1');
-    let slide = document.querySelectorAll('.slides1 li'),
-        currentIdx = 0,
-        slideCount = slide.length,
-        slideWidth = 200,
-        slideMargin = 25,
-        prevBtn = document.querySelector('.prev1'),
-        nextBtn = document.querySelector('.next1');
+        let slides = document.querySelector('.slides1');
+        let slide = document.querySelectorAll('.slides1 li'),
+            currentIdx = 0,
+            slideCount = slide.length,
+            slideWidth = 200,
+            slideMargin = 25,
+            prevBtn = document.querySelector('.prev1'),
+            nextBtn = document.querySelector('.next1');
 
-    makeClone();
+        makeClone();
 
-    function makeClone(){
-        for(let i = 0; i<slideCount;i++) {
-            let cloneSlide = slide[i].cloneNode(true);
-            cloneSlide.classList.add('clone');
-            slides.appendChild(cloneSlide);
-        }
-        for(let i = slideCount-1;i>=0;i--){
-            let cloneSlide = slide[i].cloneNode(true);
-            cloneSlide.classList.add('clone');
-            slides.prepend(cloneSlide);
-        }
-        updateWidth();
-        setInitialPos();
-        setTimeout(function (){
-            slides.classList.add('animated');
-        },100)
-    }
-
-    function updateWidth(){
-        let currentSlides = document.querySelectorAll('.slides1 li');
-        let newSlideCount = currentSlides.length;
-
-        let newwidth = (slideWidth + slideMargin) *newSlideCount - slideMargin + 'px';
-        slides.style.width = newwidth;
-    }
-
-    function setInitialPos(){
-        let initialTranslateValue = -(slideWidth + slideMargin) *slideCount;
-        slides.style.transform = 'translateX('+initialTranslateValue+'px)';
-    }
-
-    nextBtn.addEventListener('click',function (){
-        moveSlide(currentIdx + 1);
-    })
-
-    prevBtn.addEventListener('click',function (){
-        moveSlide(currentIdx - 1);
-    })
-
-    function moveSlide(num){
-        slides.style.left = - num *(slideWidth+slideMargin) +'px';
-        currentIdx = num;
-        if(currentIdx === slideCount || currentIdx === -slideCount){
-            setTimeout(function (){
-                slides.classList.remove('animated');
-                slides.style.left = '0px';
-                currentIdx = 0;
-            },500)
-            setTimeout(function (){
+        function makeClone() {
+            for (let i = 0; i < slideCount; i++) {
+                let cloneSlide = slide[i].cloneNode(true);
+                cloneSlide.classList.add('clone');
+                slides.appendChild(cloneSlide);
+            }
+            for (let i = slideCount - 1; i >= 0; i--) {
+                let cloneSlide = slide[i].cloneNode(true);
+                cloneSlide.classList.add('clone');
+                slides.prepend(cloneSlide);
+            }
+            updateWidth();
+            setInitialPos();
+            setTimeout(function () {
                 slides.classList.add('animated');
-            },600);
+            }, 100)
         }
-    }
 
+        function updateWidth() {
+            let currentSlides = document.querySelectorAll('.slides1 li');
+            let newSlideCount = currentSlides.length;
+
+            let newwidth = (slideWidth + slideMargin) * newSlideCount - slideMargin + 'px';
+            slides.style.width = newwidth;
+        }
+
+        function setInitialPos() {
+            let initialTranslateValue = -(slideWidth + slideMargin) * slideCount;
+            slides.style.transform = 'translateX(' + initialTranslateValue + 'px)';
+        }
+
+        nextBtn.addEventListener('click', function () {
+            moveSlide(currentIdx + 1);
+        })
+
+        prevBtn.addEventListener('click', function () {
+            moveSlide(currentIdx - 1);
+        })
+
+        function moveSlide(num) {
+            slides.style.left = -num * (slideWidth + slideMargin) + 'px';
+            currentIdx = num;
+            if (currentIdx === slideCount || currentIdx === -slideCount) {
+                setTimeout(function () {
+                    slides.classList.remove('animated');
+                    slides.style.left = '0px';
+                    currentIdx = 0;
+                }, 500)
+                setTimeout(function () {
+                    slides.classList.add('animated');
+                }, 600);
+            }
+        }
 
 })( jQuery, window, document );
 
